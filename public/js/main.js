db5c870e7f1a302c15690ef6645433555a1486e2
pietertolsma <epieter.tolsma@gmail.com>
2016-07-21 22:18:35 +0200

    #MakeCodePrettyAgain

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+var socket = io();
+$(document).ready(function () {
+  $('#setUsername').modal('show');
+  setTimeout(function () {
+    $("#setUsernameInput").focus();
+  }, 500);
+});
+$("#input").keypress(function (e) {
+  if(e.which == 13) sendMessage();
+});
+$("#inputButton").on('click', function () {
+  sendMessage();
+});
+
+function sendMessage () {
+  var msg = $("#input").val();
+  if(msg){
+    socket.emit('chat message', msg);
+    $('#input').val('');
+  }
+}
+
+$("#setUsernameButton").on('click', function () {changeUsername()});
+$("#setUsernameInput").keypress(function (e) {
+  if (e.which == 13) changeUsername();
+});
+
+function changeUsername () {
+  var newName = $("#setUsernameInput").val();
+  if (newName) {
+    socket.emit('username change', newName);
+    $("#setUsername").modal('hide');
+    $("#input").focus();
+  } else {
+    $("#empty-warning").show();
+  }
+}
+socket.on("chat message", function (msg) {
+  $('#chat').append($("<li>").text(msg));
+  var chat = $("#chat");
+  chat.animate({scrollTop: chat.prop("scrollHeight") - chat.height()}, 100);
+});
+
+socket.on("user change", function (count) {
+  $("#user-count").text(count);
+});
+
+socket.on("chat history", function (messages) {
+  var msgArray = messages.split(',');
+  for (var i = 0; i < msgArray.length; i++) {
+    $('#chat').append($("<li>").text(msgArray[i]));
+  }
+  var chat = $("#chat");
+  chat.animate({scrollTop: chat.prop("scrollHeight") - chat.height()}, 100);
+});
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-var fs = require('fs');
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//=============== Initialization ===============================================================
 
-fs.readFile('package.json', 'utf8', function(err, data){
-  if(err) {
+var fs = require('fs'); //Handles all the reading / writing files
+var express = require('express'); //Makes things like routing easier
+var app = express(); //Get express object, see express docs
+var http = require('http').Server(app); //Used to listen for incoming requests
+var io = require('socket.io')(http); //The secret sauce of this application, Socket.IO. Handles TCP connections
+
+//Read from the config file 'package.json'
+fs.readFile('package.json', 'utf8', function (err, data) {
+  if (err) {
     console.error("Failed to read config.json!", err);
   }
-  startServer(JSON.parse(data));
+  startServer(JSON.parse(data)); //Start the server when all data is read
 });
 
-var connectedClients = [];
-var chatBackLog = [];
-var maxBackLogSize = 0; //Default
+var connectedClients = []; //This array will be filled up with data from connected users.
+var chatBackLog = []; //This array will be filled with the most recent chat messages.
+var maxBackLogSize = 0; //This is the max size of the chat history, when cap is reached oldest will be removed.
+
+//================= Getters + Setters ==========================================================
 
 function getUsername (sessionId) {
   for (var i = 0; i < connectedClients.length; i++) {
@@ -21,20 +26,21 @@ function getUsername (sessionId) {
   }
 }
 
-function findUserIndex (sessionId) {
+function getUserIndex (sessionId) {
   for (var i = 0; i < connectedClients.length; i++) {
     if (connectedClients[i].sessionId === sessionId) return i;
   }
 }
 
-function startServer (data) {
-  maxBackLogSize = data.maxChatHistory;
-  //Include static files such as css and js
-  app.use(express.static(__dirname + '/public'));
+//================ Main Server Method ============================================================
 
-  //Creates the server (routing)
+function startServer (data) {
+  maxBackLogSize = data.maxChatHistory; //Set the maximum chat history
+  app.use(express.static(__dirname + '/public')); //Makes sure that client has access to all public files
+
+  //Create a server route on root ('/')
   app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/index.html"); //Send the homepage to client
   });
 
   //Fires when a new client connects
@@ -44,30 +50,35 @@ function startServer (data) {
     io.emit('chat history', chatBackLog.toString());
     io.emit('user change', connectedClients.length);
 
-    //Fires when the a client sends a chat message
+    //Fires when a client sends a chat message
     socket.on('chat message', function (msg) {
-      var fullMessage = getUsername(socket.id) + msg;
-      if (chatBackLog.length >= maxBackLogSize) chatBackLog.splice(0, 1);
-      chatBackLog.push(fullMessage);
-      io.emit('chat message', fullMessage);
+      if (msg) {
+        var fullMessage = getUsername(socket.id) + msg;
+        if (chatBackLog.length >= maxBackLogSize) chatBackLog.splice(0, 1);
+        chatBackLog.push(fullMessage);
+        io.emit('chat message', fullMessage);
+      }
     });
 
+    //Fires when a user changes its username
     socket.on('username change', function (name) {
-      connectedClients[findUserIndex(socket.id)].name = "[" + name + "] ";
-      io.emit('chat message', getUsername(socket.id) + " has joined the channel.");
+      if (name) {
+        connectedClients[getUserIndex(socket.id)].name = "[" + name + "] ";
+        io.emit('chat message', getUsername(socket.id) + " has joined the channel.");
+      }
     });
 
     //Fires when a user disconnects
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
       console.log("A user disconnected!");
       io.emit('chat message', getUsername(socket.id) + " has left the channel.");
-      connectedClients.splice(findUserIndex(socket.id), 1);
+      connectedClients.splice(getUserIndex(socket.id), 1);
       io.emit('user change', connectedClients.length);
     });
   });
 
   //Start listening for incoming data
-  http.listen(data.port, function(){
+  http.listen(data.port, function () {
     console.log(data.name + " " + data.version + " started on port " + data.port);
   });
 }
